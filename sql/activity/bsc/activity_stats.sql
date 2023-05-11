b3e835f16243e620ab8cbfae6ca5a5a40a296611
gronders <grahamonders@gmail.com>
2023-05-11 13:15:30 -0600

    remove a pesky comma

--- sql/activity/bsc/activity_stats.sql
+++ sql/activity/bsc/activity_stats.sql
@@ -4,7 +4,7 @@ count(DISTINCT(date_trunc('DAY', block_timestamp))) as n_days_active,
 DATEDIFF(day, MAX(BLOCK_TIMESTAMP), CURRENT_DATE()) as days_since_last_txn,
 -- a complex tx is any tx that is NOT a simple ETH transfer; i.e., has input data!
   count_if(INPUT_DATA != '0x') as n_complex_txn,
-COUNT (DISTINCT CASE WHEN INPUT_DATA != '0x' THEN TO_ADDRESS END) as n_contracts,
+COUNT (DISTINCT CASE WHEN INPUT_DATA != '0x' THEN TO_ADDRESS END) as n_contracts
 FROM bsc.core.fact_transactions
 WHERE 
 BLOCK_TIMESTAMP >= DATEADD('day', 
