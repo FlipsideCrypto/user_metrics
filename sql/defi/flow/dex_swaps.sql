b2add4ce1ebb162656318125a6f6840c21275d03
gronders <grahamonders@gmail.com>
2023-05-11 13:21:48 -0600

    update flow dex swap price table reference

--- sql/defi/flow/dex_swaps.sql
+++ sql/defi/flow/dex_swaps.sql
@@ -1,11 +1,11 @@
 WITH daily_prices AS (
 SELECT 
-  symbol,
-  token_contract,
-  date_trunc('day', timestamp) AS day,
-  AVG(price_usd) AS price
-FROM flow.core.fact_prices
-WHERE timestamp > current_date - 91
+  token as symbol,
+  id as token_contract,
+  date_trunc('day', recorded_hour) AS day,
+  AVG(close) AS price
+FROM flow.core.fact_hourly_prices
+WHERE recorded_hour > current_date - 91
 GROUP BY symbol, token_contract, day
 ),
 sells AS (
